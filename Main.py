b853fefaf68d1869010c2006a3c1e645e8e8ceeb
Ayush Goyal <56213076+Mr-Anon@users.noreply.github.com>
2021-05-11 15:57:12 +0530

    Reduced API calls to 1 from 5
    Date part is also handled now

--- Main.py
+++ Main.py
@@ -7,8 +7,8 @@ import os
 import winsound
 import pyttsx3
 
-age = 45
-district_id = 506
+age = 18
+district_id = 149
 while True:
     
     Vaccine_found = False
@@ -22,13 +22,8 @@ while True:
     # dd/mm/YY
 
     d1 = str(day) + "-0" + str(month) + "-" + str(year)
-    d2 = str(day+1) + "-0" + str(month) + "-" + str(year)
-    d3 = str(day+2) + "-0" + str(month) + "-" + str(year)
-    d4 = str(day+3) + "-0" + str(month) + "-" + str(year)
-    d5 = str(day+4) + "-0" + str(month) + "-" + str(year)
 
     
-    print(d1)
     
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' , 'accept': 'application/json', 'Accept-Language': 'en_IN' }
@@ -37,110 +32,27 @@ while True:
         # b = json.load(a)
 
         for center in a['centers']:
-            for sessions in center['sessions']:
-                if sessions['available_capacity'] > 0:
-                    if sessions['min_age_limit'] == age:
+            for session in center['sessions']:
+                if session['available_capacity'] > 0:
+                    if session['min_age_limit'] == age:
+                        print(session['date'])
                         print(center)
                         Vaccine_found = True
                         engine = pyttsx3.init()
-                        engine.say("Vaccine is avaliable at " + str(center['name']))
+                        engine.say("Vaccine is available at " + str(center['name']))
                         engine.runAndWait()
+                
         time.sleep(2)
-        print(d2)
-    except:
-        pass
-
-
-    
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' , 'accept': 'application/json', 'Accept-Language': 'en_IN' }
-        B= requests.get("http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+str(district_id)+"&date="+d2, headers=headers).json()
-
-        # b = json.load(a)
-
-        for center in B['centers']:
-            for sessions in center['sessions']:
-                if sessions['available_capacity'] > 0:
-                    if sessions['min_age_limit'] == age:
-                        print(center)
-                        Vaccine_found = True
-                        engine = pyttsx3.init()
-                        engine.say("Vaccine is avaliable at " + str(center['name']))
-                        engine.runAndWait()
-
-        time.sleep(2)
-        print(d3)
-
-    except:
-        pass
-
-    
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' , 'accept': 'application/json', 'Accept-Language': 'en_IN' }
-        C= requests.get("http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+str(district_id)+"&date="+d3, headers=headers).json()
-
-        # b = json.load(a)
-
-        for center in C['centers']:
-            for sessions in center['sessions']:
-                if sessions['available_capacity'] > 0:
-                    if sessions['min_age_limit'] == age:
-                        print(center)
-                        Vaccine_found = True
-                        engine = pyttsx3.init()
-                        engine.say("Vaccine is avaliable at " + str(center['name']))
-                        engine.runAndWait()
-
-        time.sleep(2)
-        print(d4)
-    except:
-        pass
-    
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' , 'accept': 'application/json', 'Accept-Language': 'en_IN' }
-        D= requests.get("http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+str(district_id)+"&date="+d4, headers=headers).json()
-
-        # b = json.load(a)
-
-        for center in D['centers']:
-            for sessions in center['sessions']:
-                if sessions['available_capacity'] > 0:
-                    if sessions['min_age_limit'] == age:
-                        print(center)
-                        Vaccine_found = True
-                        engine = pyttsx3.init()
-                        engine.say("Vaccine is avaliable at " + str(center['name']))
-                        engine.runAndWait()
-
-        time.sleep(2)
-        print(d5)
-    except:
-        pass
-    
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36' , 'accept': 'application/json', 'Accept-Language': 'en_IN' }
-        E= requests.get("http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+str(district_id)+"&date="+d5, headers=headers).json()
-
-        # b = json.load(a)
-
-        for center in E['centers']:
-            for sessions in center['sessions']:
-                if sessions['available_capacity'] > 0:
-                    if sessions['min_age_limit'] == age:
-                        print(center)
-                        Vaccine_found = True
-                        engine = pyttsx3.init()
-                        engine.say("Vaccine is avaliable at " + str(center['name']))
-                        engine.runAndWait()
-    
-    except:
+    except Exception as e:
+        print(e)
         pass
     
     if Vaccine_found:
-        winsound.Beep(800, 3000)
+        winsound.Beep(800, 6000)
     else:
-        time.sleep(30)
-    os.system('cls')
+        print("No Vaccine Found")
+        time.sleep(5)
+        os.system('cls')
 
 
 
